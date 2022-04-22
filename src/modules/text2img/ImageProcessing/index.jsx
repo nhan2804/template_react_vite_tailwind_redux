a5d80443245572f5873586017089f2128fd955a5
Huy19IT1 <huy2001nd@gmail.com>
2022-04-22 19:15:09 +0700

    update UI ImgProccessing

--- src/modules/text2img/ImageProcessing/index.jsx
+++ src/modules/text2img/ImageProcessing/index.jsx
@@ -9,6 +9,7 @@ import {
   Space,
   Switch,
   Tag,
+  Layout,
 } from "antd";
 import React, { useEffect, useMemo, useState } from "react";
 import { useRef } from "react";
@@ -18,7 +19,7 @@ import useImageProcessing, { typeIP } from "../hooks/useImageProcessing";
 const { Option } = Select;
 function ImageProcessing(props) {
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
-
+  const { Header, Content, Footer } = Layout;
   const [file, setstate] = useState();
   const [model, setMode] = useState(false);
   const [filter, SetFilter] = useState();
@@ -38,30 +39,105 @@ function ImageProcessing(props) {
   const dataFilterImage = useMemo(() => {
     switch (type) {
       case typeIP.brightNess:
-        const marks = {
-          0: "100",
-
-          100: "150",
-        };
+        const marksB = {
+          100: {label: <strong>100</strong>,}, 
+          150: {label: <strong>150</strong>,}, 
+        };  
         return (
-          <Form.Item label="Val" name="val">
-            <Slider marks={marks} defaultValue={30} min={100} max={150} />
+          <Form.Item label={'Val'} name="val" >
+            <Slider marks={marksB} initialValue={100} min={100} max={150} />
           </Form.Item>
         );
+        break;
       case typeIP.tv60:
+          const marksV = {
+            0: {label: <strong>0</strong>,}, 
+            255: {label: <strong>255</strong>,}, 
+          };  
+          const marksT = {
+            0: {label: <strong>0</strong>,}, 
+            100: {label: <strong>100</strong>,}, 
+          };  
+          return (
+            <>
+              <Form.Item label={'Val'} name="val">
+                <Slider marks={marksV} initialValue={0} min={0} max={255} />
+              </Form.Item>
+              <Form.Item label={'Threshold'} name="thresh">
+                <Slider marks={marksT} initialValue={0} min={0} max={100} />
+              </Form.Item>
+            </>
+          );
+        break;
+      case typeIP.emBoss:
+        const size = {
+          0: {label: <strong>0</strong>,}, 
+          8: {label: <strong>8</strong>,}, 
+        };  
+        const sw = {
+          0: {label: <strong>0</strong>,}, 
+          1: {label: <strong>1</strong>,}, 
+          2: {label: <strong>2</strong>,}, 
+          3: {label: <strong>3</strong>,}, 
+        };  
         return (
           <>
-            <Form.Item label="Val" name="val">
-              <Slider defaultValue={0} min={0} max={255} />
+            <Form.Item label="Size" name="size">
+              <Slider marks={size} initialValue={0}  min={0} max={8} />
             </Form.Item>
-            <Form.Item label="Thresh" name="thresh">
-              <Slider defaultValue={0} min={0} max={100} />
+            <Form.Item label="0 : BL, 1 : BR, 2 : TR, 3 : BR" name="switch">
+              <Slider marks={sw} initialValue={0} step={1} min={0} max={3} />
             </Form.Item>
           </>
         );
-
         break;
-
+      case typeIP.duoTone:
+          const exp = {
+            0: {label: <strong>0</strong>,}, 
+            10: {label: <strong>10</strong>,}, 
+          };  
+          const s1 = {
+            0: {label: <strong>BLUE</strong>,}, 
+            1: {label: <strong>GREEN</strong>,}, 
+            2: {label: <strong>RED</strong>,}, 
+          };
+          const s2 = {
+            0: {label: <strong>BLUE</strong>,}, 
+            1: {label: <strong>GREEN</strong>,}, 
+            2: {label: <strong>RED</strong>,}, 
+            3: {label: <strong>NONE</strong>,}, 
+          };    
+          const s3 = {
+            0: {label: <strong>DARK</strong>,}, 
+            1: {label: <strong>LIGHT</strong>,}, 
+          };  
+          return (
+            <>
+              <Form.Item label="Exponent" name="exponent">
+                <Slider marks={exp} initialValue={0}  min={0} max={10} />
+              </Form.Item>
+              <Form.Item label="0 : BLUE, 1 : GREEN, 2 : RED" name="s1">
+                <Slider marks={s1} initialValue={0} step={1} min={0} max={2} />
+              </Form.Item>
+              <Form.Item label="0 : BLUE, 1 : GREEN, 2 : RED, 3 : NONE" name="s2">
+                <Slider marks={s2} initialValue={0} step={1} min={0} max={3} />
+              </Form.Item>
+              <Form.Item label="0 : DARK, 1 : LIGHT" name="s3">
+                <Slider marks={s3} initialValue={0} step={1} min={0} max={1} />
+              </Form.Item>
+            </>
+          );
+          break;
+      case typeIP.sepia:
+        return (
+          true
+        );
+        break;
+      case typeIP.negative:
+        return (
+          true
+        );
+        break;
       default:
         break;
     }
@@ -79,6 +155,7 @@ function ImageProcessing(props) {
     reset();
   };
   const onHandleImage = () => {
+
     const formData = new FormData();
     formData.append("file", acceptedFiles?.[0]);
     if (fileNameImgP && model) {
@@ -103,10 +180,13 @@ function ImageProcessing(props) {
     setType(type);
   };
   return (
-    <>
+    <Layout>
+    <Content style={{ padding: '0 50px' }}>
       <Row>
-        <Col span={12} className="flex justify-center items-center mt-40">
-          <section className="container flex justify-center spacing-2 items-center">
+        <Col span={24} className="flex justify-center items-center mt-10">
+          <Row>
+            <Col span={12}>
+            <section className="container flex justify-center spacing-2 items-center">
             <div className="">
               <div {...getRootProps({ className: "dropzone" })}>
                 <input {...getInputProps()} />
@@ -120,25 +200,35 @@ function ImageProcessing(props) {
               </div>
             </div>
           </section>
-          <Switch
-            defaultChecked={model}
-            onChange={(checked) => setMode(checked)}
-          />
-          {file && <h4 className="ml-12">Files</h4>}
+          
+          {/* {file && <h4 className="ml-12">Files</h4>} */}
           {/* <ul className="grid grid-cols-6 gap-4 p-2 ml-10">{files}</ul> */}
-          <div className="flex justify-center">
-            <div className="flex space-x-3">
+            </Col>
+            <Col span={12}>
+              <Row>
+                <Col span={24}>
+                  <h4>Use continous</h4> 
+                   <Switch
+                        defaultChecked={model}
+                        onChange={(checked) => setMode(checked)}
+                      />
+                </Col>
+              </Row>
+              <Row>
+              <Col span={24} className="mt-5">
+              <h4>Option</h4>
               <Space>
                 <Select
-                  defaultValue="sharp"
+                  defaultValue={null}
                   style={{ width: 120 }}
                   onChange={handleChange}
                 >
-                  <Option value="emboss">Emboss</Option>
                   <Option value="brightness">Brightness</Option>
+                  <Option value="emboss">Emboss</Option>
+                  <Option value="duo-tone">DuoTone</Option>
                   <Option value="tv-60">Tv-60</Option>
                   <Option value="sepia">Sepia</Option>
-                  <Option value="duo-tone">duo-tone</Option>
+                  <Option value="negative">Negative</Option>
                 </Select>
 
                 <Button
@@ -159,52 +249,71 @@ function ImageProcessing(props) {
                   Reset
                 </Button>
               </Space>
-            </div>
-          </div>
-          <div className="w-96d text-center p-4">
-            <Form
-              onValuesChange={(changedValues, allValues) => {
-                console.log("changed");
-                SetFilter((s) => allValues);
-              }}
-              form={form}
-              layout=""
-              autoComplete="off"
-            >
-              {dataFilterImage}
-            </Form>
-          </div>
+
+              </Col>
+              </Row>
+              <Row>
+              <Col span={24} className="mt-5">
+              <Form
+               layout="vertical"
+                onValuesChange={(changedValues, allValues) => {
+                  console.log("changed");
+                  SetFilter((s) => allValues);
+                }}
+                form={form}
+                autoComplete="off"
+              >
+                {dataFilterImage}
+              </Form>
+              </Col>
+              </Row>
+            </Col>
+          </Row>
         </Col>
 
-        <Col span={12} className="mt-20">
+        <Col span={24} className="mt-10 mb-20">
           {dataImageP && (
             <h1 className="text-center text-3xl">
               {" "}
               {type?.toUpperCase()} IMAGE
             </h1>
           )}
-          <div className="flex justify-center mr-14">
-            <div className="flex space-x-1">
-              <div>
-                <h1 className="text-center text-2xl">Before</h1>
-                <img
-                  className="w-[500px]"
+          <Row>
+            <Col span={12}>
+            <div>
+                {file && <h1 className="text-center text-2xl">Before</h1>}
+                {/* <h1 className="text-center text-2xl">Before</h1> */}
+                <div className="w-full flex justify-center items-center">
+                  <img
+                  style={{ 
+                    width:'99%'
+                   }}
                   alt=""
                   src={file ? URL.createObjectURL(file) : ""}
                 />
+                </div>
+                
               </div>
-              <div>
-                <h1 className="text-center text-2xl">After</h1>
+            </Col>
+            <Col span={12}>
+            <div>
+            {file && <h1 className="text-center text-2xl">After</h1>}
+                {/* <h1 className="text-center text-2xl">After</h1> */}
                 {dataImageP ? (
-                  <div className="w-[500px]">
-                    <img className="w-full" alt="" src={dataImageP} />
+                  <div className="w-full flex justify-center items-center">
+                    <img  style={{ 
+                    width:'99%'
+                   }}
+                    alt="" src={dataImageP} />
                   </div>
                 ) : (
-                  <div className="w-[500px]">
+                  <div className="w-full flex justify-center items-center">
                     {file ? (
                       // <Skeleton.Image paragraph={4} active loading />
                       <img
-                        className="w-full"
+                         style={{ 
+                            width:'99%',
+                          }}
                         alt=""
                         src={file ? URL.createObjectURL(file) : ""}
                       />
@@ -214,11 +323,13 @@ function ImageProcessing(props) {
                   </div>
                 )}
               </div>
-            </div>
-          </div>
+            </Col>
+          </Row>
         </Col>
       </Row>
-    </>
+    </Content>
+    </Layout>
+
   );
 }
 export default ImageProcessing;
